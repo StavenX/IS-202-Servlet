c93b287946d49a5242e2b55003f96d7bb5a8cbf8
Tobias 720s <tobiasalbert97@gmail.com>
2018-10-01 19:11:39 +0200

    added new files

---
+++ src/java/helpers/HtmlHelper.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helpers;
+
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author Tobias
+ */
+public class HtmlHelper {
+    private PrintWriter out;
+    
+    
+    public HtmlHelper (PrintWriter out) {
+        this.out = out;
+    }
+    
+    public void printHead (String title, String bodyId) {
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<meta charset=\"UTF-8\">");
+        out.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"theme.css\">");
+        out.println("<title>Servlet getStudent</title>");            
+        out.println("</head>");
+        out.println("<body>");
+    }
+    
+    public void printEnd () {
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
