b30bb3276d61c841f714755e3afc916d00348e50
Unknown <adrian.nesvik@lyse.net>
2018-11-01 22:16:04 +0100

    old chum

--- web/js_Calendars.js
+++ web/js_Calendars.js
@@ -1,5 +1,30 @@
+var currentDate = new Date();
+var lastDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth()+1,0);
+var monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November","December"];
+var weekdayNames = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
+function monthName() {
+    document.getElementById("boi").innerHTML += ("<td>" + monthNames[currentDate.getMonth()] + "</td>");
+}
 
-for(var q = 0; q <= 3; q++) {
+function weekdaysName() {
     
-};
+}
+function calendar() {
+    document.getElementById("boi2").innerHTML += ("<table><tr><td colspan=7>" + monthNames[currentDate.getMonth()] + "<tr>");
+    for(var p = 0; p < weekdayNames.length; p++) {
+    document.getElementById("boi2").innerHTML += ("<td>" + weekdayNames[p] + "</td>");
+    }
+    document.getElementById("boi2").innerHTML += ("</tr><tr>");
+    for(var w = 1; w <= lastDayOfMonth.getDate(); w++) {
+        document.getElementById("boi2").innerHTML += ("<td>" + w + "</td>");
+    }
+}
+function calendars() {
+    for(var w = 1; w <= lastDayOfMonth.getDate(); w++) {
+        if(w % 7 === 0) {
+            document.getElementdByTagName("tbody").innerHTML += ("</tr><tr class=\"boi2\">");
+        }
+        document.getElementsByTagName("tbody").innerHTML += ("<td>" + w + "</td>");
+    }
+}
