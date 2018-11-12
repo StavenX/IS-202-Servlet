8f2ad992ab65e6c6a7f4782558519eaedb45b726
Unknown <edvin_myhren@hotmail.com>
2018-11-12 22:23:49 +0100

    Added new file-upload (super early stage and unstable as fuck)
    - Uses a database instead of local directory
    - Uses helper classes inside newfileupload package

--- src/java/servlets/serv_FileUpload.java
+++ src/java/servlets/serv_FileUpload.java
@@ -1,28 +1,21 @@
 package servlets;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
 import helpers.HtmlHelper;
+import javax.servlet.annotation.MultipartConfig;
+import newfileupload.UploadFile; 
 
 @WebServlet(name = "serv_FileUpload", urlPatterns = { "/FileUpload" }, loadOnStartup = 1)
-@MultipartConfig(
-                fileSizeThreshold = 6291456,    // 6 MB
-		maxFileSize =       10485760L,  // 10 MB
-		maxRequestSize =    20971520L   // 20 MB
-)
+@MultipartConfig
 
 public class serv_FileUpload extends HttpServlet {
 
-	private static final String UPLOAD_DIR = "uploads";
-        
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
@@ -42,59 +35,32 @@ public class serv_FileUpload extends HttpServlet {
                         out.println("STUDENT_ACCESS");
                     }
                        
-                    out.append("<form method=\"POST\" action=\"FileUpload\" ")
-                    .append("enctype=\"multipart/form-data\">\r\n");
-                    out.append("<input class=\"button\" type=\"file\" name=\"fileName1\"/><br/><br/>\r\n");
-                    //writer.append("<input type=\"file\" name=\"fileName2\"/><br/><br/>\r\n");
-                    out.append("<input class=\"button\" type=\"submit\" value=\"Submit\"/>\r\n");
-                    out.append("</form>\r\n");
-
+                    // This is the form that contains the upload button, etc. 
+                    out.println("<form method='POST' action='UploadFile' enctype='multipart/form-data'>");
+                    out.println("<table>");
+                    out.println("<tr>");
+                    out.println("<th>Select File:</th>");
+                    out.println("<td><input type='file' name='file'/></td>");
+                    out.println("</tr>");
+                    out.println("<tr>");
+                    out.println("<td colspan='2'>");
+                    out.println("<input type='submit' value='Upload selected file'>");
+                    out.println("</td>");
+                    out.println("</tr>");
+                    out.println("</table>");
+                    out.println("</form>");
+                    
                     site.printEnd();
-                }
-		
-		
+                }	
 	}
-	@Override
+        
+	/*@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            
 		response.setContentType("text/html");
 		response.setCharacterEncoding("UTF-8");
                 
-		// gets absolute path of the web application
-		String applicationPath = request.getServletContext().getRealPath("");
-		// constructs path of the directory to save uploaded file
-		String uploadFilePath = applicationPath + File.separator + UPLOAD_DIR;
-                
-		// creates upload folder if it does not exists
-		File uploadFolder = new File(uploadFilePath);
-		if (!uploadFolder.exists()) {
-			uploadFolder.mkdirs();
-		}
-                
-		PrintWriter writer = response.getWriter();
-                
-                // write all files in upload folder / location
-                for (Part part : request.getParts()) {
-                    if (part != null && part.getSize() > 0) {
-                        String fileName = part.getSubmittedFileName();
-                        String contentType = part.getContentType();
-
-                        // allows only JPEG files to be uploaded, might
-                                        // be useful later
-                        /*if (!contentType.equalsIgnoreCase("image/jpeg")) {
-                            continue;
-                        }*/
-
-                                        // To fix Microsoft Edge handling filenames
-                                        String[] fileNameSplit = fileName.split("\\\\");
-                        fileName = fileNameSplit[fileNameSplit.length - 1];
-                        part.write(uploadFilePath + File.separator + fileName);
-
-                        writer.append("File successfully uploaded to "
-                                + uploadFolder.getAbsolutePath()
-                                + File.separator
-                                + fileName
-                                + "<br>\r\n");
-                    }
-                }     
-	}
+		UploadFile uf = new UploadFile(); 
+                uf.processRequest(request, response);
+	}*/
 }
