c24d788573933c842b7a9886db6198077ca3dab9
Adrian <adrian.nesvik@lyse.net>
2018-11-16 09:46:29 +0100

    Small changes

--- src/java/servlets/serv_Calendar.java
+++ src/java/servlets/serv_Calendar.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import network.Login;
 
 /**
  *
@@ -36,5 +37,6 @@ String Dawg = "doggiedog";
      @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
     }
 }
