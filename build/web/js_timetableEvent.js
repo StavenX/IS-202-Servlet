d2a0c313e7d1703b265aaf0b3e24a389811d1cfa
Unknown <adrian.nesvik@lyse.net>
2018-11-26 23:30:35 +0100

    shit merge 2

--- build/web/js_timetableEvent.js
+++ build/web/js_timetableEvent.js
@@ -1,2 +1,10 @@
-$('#01110900').html('background-color:white');
-$('.hi').html('bye');
+$(document).ready(function() {
+    $("#201829101000").replaceWith( 
+                    "<td class=\" test-events\" rowspan=\"4\">\n" +
+                    "<span class=\"clockStart\">10:00</span><br>\n" +
+                    "<span class=\"clockEnd\">10:45</span>\n" +
+                    "<span class=\"title\">test subject</span><br>\n" +
+                    "<span class=\"lecturer\">test lecturer</span><br>\n" +
+                    "<span class=\"class\">test class 3421</span>\n" +
+                    "</td>");
+});
