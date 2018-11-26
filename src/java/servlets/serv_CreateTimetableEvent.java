a79e3bc732518f846685fb1db07232db62cba22f
Unknown <adrian.nesvik@lyse.net>
2018-11-26 19:37:55 +0100

    Displays current day and weekday + more
    - Option to chose from db courses when creating new event

--- src/java/servlets/serv_CreateTimetableEvent.java
+++ src/java/servlets/serv_CreateTimetableEvent.java
@@ -1,13 +1,18 @@
 package servlets;
 
+import helpers.CalendarHelper;
 import helpers.HtmlHelper;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import network.Login;
+import helpers.CourseHelper;
+import java.sql.ResultSet;
 
 /**
  *
@@ -15,7 +20,9 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "serv_CreateTimetableEvent", urlPatterns = {"/CreateTimetableEvent"})
 public class serv_CreateTimetableEvent extends HttpServlet {
-
+    
+    Login dbLogin = new Login();
+    
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -32,12 +39,25 @@ public class serv_CreateTimetableEvent extends HttpServlet {
         try (PrintWriter out = response.getWriter()) {
             
             HtmlHelper site = new HtmlHelper(out);
+            
             site.printHead("New event", "body");
-            
-            
-            // Form for new event input
             out.println("<form method=\"post\">");
-            out.println("Class<br><input type=\"text\" name=\"class\"><br><br>");
+            out.println("Course code<br><select name=courseCode><br><br>");
+            Connection conn;
+            conn = dbLogin.loginToDB(out);
+            
+            try {
+                ResultSet rset = CourseHelper.getAllCourses(out, conn);
+                while (rset.next()) {
+                    out.println("<option value=" + (rset.getString("course_id")) + ">" + (rset.getString("course_name")) + "</option>");
+                }
+            }
+            catch (Exception e) {
+                out.println(e);
+            }
+            dbLogin.close();
+            
+            out.println("</select><br><br>");
             out.println("Classroom<br><input type=\"text\" name=\"classroom\"><br><br>");
             out.println("Lecturer(s)<br><input type=\"text\" name=\"lecturer\"><br><br>");
             out.println("Start time<br><input type=\"time\" name=\"st\"><br><br>");
@@ -82,30 +102,23 @@ public class serv_CreateTimetableEvent extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet serv_CreateTimetableEvent</title>");  
-            out.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"theme.css\">");
-            out.println("</head>");
-            out.println("<body>");
+            HtmlHelper site = new HtmlHelper(out);
+            site.printHead("New event", "body");
             
-            request.getParameter("class");
-            request.getParameter("classroom");
-            request.getParameter("lecturer");
-            request.getParameter("st");
-            request.getParameter("et");
-            request.getParameter("Monday");
-            request.getParameter("Tuesday");
-            request.getParameter("Wednesday");
-            request.getParameter("Thursday");
-            request.getParameter("Friday");
-            request.getParameter("Saturday");
-            request.getParameter("Sunday");
-            request.getParameter("note");
-            
-            out.println("</body>");
-            out.println("</html>");
+            out.println(request.getParameter("courseCode"));
+            out.println(request.getParameter("classroom"));
+            out.println(request.getParameter("lecturer"));
+            out.println(request.getParameter("st"));
+            out.println(request.getParameter("et"));
+            out.println(request.getParameter("Monday"));
+            out.println(request.getParameter("Tuesday"));
+            out.println(request.getParameter("Wednesday"));
+            out.println(request.getParameter("Thursday"));
+            out.println(request.getParameter("Friday"));
+            out.println(request.getParameter("Saturday"));
+            out.println(request.getParameter("Sunday"));
+            out.println(request.getParameter("note"));
+
         }
     }
 }
