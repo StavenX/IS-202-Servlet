fdbff0dcc5a7de04bfcbc9308d12e7a593febc5d
Unknown <frankthorsenespen@hotmail.com>
2018-11-27 10:42:18 +0100

    some msg fix and sidebar

--- src/java/helpers/MessageHelper.java
+++ src/java/helpers/MessageHelper.java
@@ -52,7 +52,7 @@ public class MessageHelper {
             // The button that prints all messages
             out.println(
                 "<form action=\"getMessage\" method=\"post\">\n" +
-"                   <input class=\"button\" type=\"Submit\" name=\"get\" value=\"Get all Messages from Database\">   \n" +
+"                   <input class=\"button\" type=\"Submit\" name=\"get\" value=\"My messages\">   \n" +
 "               </form>");
         }
         catch (SQLIntegrityConstraintViolationException ex) {
@@ -118,7 +118,9 @@ public class MessageHelper {
                 out.println("</form>");
                 
                 //delete buttons
-                out.println("<div class=\"message-container-item\">");                
+                out.println("<form method='Get' action='Message'>" + "<input class=\"button\" type=\"Submit\" name=\"get\" value=\"Respond\">\n");
+                out.println("<div class=\"message-container-item\">");   
+
                 out.println("<form name=\"delete-form-" + mess_id + "\" action=\"deleteMessage\">");
                 site.printDeleteButton("deleteMessage", "mess_id", mess_id);
                 out.println("</div>");
